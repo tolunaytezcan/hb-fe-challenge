97ab36c70e604807549177e55b1f96b328d7831c
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-03 16:06:29 +0300

    Add search result style

--- src/components/organism/SearchResult/SearchResult.js
+++ src/components/organism/SearchResult/SearchResult.js
@@ -2,12 +2,16 @@ import SideBar from 'components/organism/SearchResult/SideBar/SideBar';
 import ProductList from 'components/organism/SearchResult/ProductList/ProductList';
 import SubHeader from 'components/organism/SearchResult/SubHeader/SubHeader';
 
+import style from 'components/organism/SearchResult/SearchResult.module.scss';
+
 const SearchResult = () => {
   return (
-    <div>
+    <div className={style.searchResultWrapper}>
       <SubHeader />
-      <SideBar />
-      <ProductList />
+      <div className={style.filterAndProducts}>
+        <SideBar />
+        <ProductList />
+      </div>
     </div>
   );
 };
