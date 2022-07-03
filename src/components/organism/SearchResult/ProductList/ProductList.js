263ae715cde390cc9dc08045a32d47a78ac33c23
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-03 16:09:30 +0300

    Add product list organism

--- src/components/organism/SearchResult/ProductList/ProductList.js
+++ src/components/organism/SearchResult/ProductList/ProductList.js
@@ -1,12 +1,18 @@
 import ProductCard from 'components/molecules/ProductCard/ProductCard';
+import { useProducts } from 'hooks';
+
+import styles from 'components/organism/SearchResult/ProductList/ProductList.module.scss';
 
 const ProductList = () => {
+  const { products, isLoading, isError } = useProducts();
+
   return (
-    <div>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div className={styles.productListContainer}>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error...</div>}
+      {products?.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
