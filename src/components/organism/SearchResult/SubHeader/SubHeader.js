6ed5ff95c15ac9103aee2bf8f25945c56048004a
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-03 16:08:54 +0300

    Add subheader organism

--- src/components/organism/SearchResult/SubHeader/SubHeader.js
+++ src/components/organism/SearchResult/SubHeader/SubHeader.js
@@ -1,5 +1,23 @@
+import Button from 'components/atoms/Button/Button';
+
+import DownArrow from 'assets/DownArrow.svg';
+import style from 'components/organism/SearchResult/SubHeader/SubHeader.module.scss';
+
 const SubHeader = () => {
-  return <div>SubHeader</div>;
+  return (
+    <div className={style.SubHeaderContainer}>
+      <div className={style.SubHeader}>
+        <span className={style.PageTitle}>iPhone iOS cep telefonu</span>
+        <span className={style.SearchTextContainer}>
+          <span>{'Aranan Kelime: '}</span>
+          <span className={style.SearchText}>iphone 11</span>
+        </span>
+      </div>
+      <div className={style.SortingButtonContainer}>
+        <Button name="Sırala" icon={DownArrow}></Button>
+      </div>
+    </div>
+  );
 };
 
 export default SubHeader;
