45bfafd9aaa3de80f5bf4f772a695f7950a1426d
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-03 16:05:02 +0300

    Remove fetch function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { useEffect } from 'react';
-
 import Header from 'components/organism/Header/Header';
 import SearchResult from 'components/organism/SearchResult/SearchResult';
-import { fetchData } from 'api';
 
 import 'styles/globals.scss';
 
 const App = () => {
-  const data = [];
-
-  useEffect(() => {
-    !data.length && fetchData();
-  }, [data]);
-
   return (
     <>
       <Header />
