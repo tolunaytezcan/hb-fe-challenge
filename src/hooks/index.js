94f2e5378a95029e154ea767819fe1a366de1d16
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-04 15:53:56 +0300

    Add useDisclosure hook for modal visibility

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useCallback } from 'react';
 import ProductsContext from 'contexts/ProductsContext';
 import FilterContext from 'contexts/FilterContext';
 import CartContext from 'contexts/CartContext';
@@ -8,3 +8,23 @@ export const useProducts = () => useContext(ProductsContext);
 export const useFilter = () => useContext(FilterContext);
 
 export const useCart = () => useContext(CartContext);
+
+export const useDisclosure = isOpenInit => {
+  const [isOpen, setIsOpen] = useState(!!isOpenInit);
+
+  const onOpen = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const onToggle = useCallback(() => {
+    setIsOpen(prev => !prev);
+  }, []);
+
+  return { isOpen, onOpen, onClose, onToggle };
+};
+
+export default useDisclosure;
