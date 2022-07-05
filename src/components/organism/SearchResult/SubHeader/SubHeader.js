f501f2a5773995713eb07975e667cb0b57134bd4
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-05 03:12:45 +0300

    Fix subheader static title

--- src/components/organism/SearchResult/SubHeader/SubHeader.js
+++ src/components/organism/SearchResult/SubHeader/SubHeader.js
@@ -1,15 +1,21 @@
 import Selectbox from 'components/molecules/Selectbox/Selectbox';
+import { useFilter } from 'hooks';
 
 import style from 'components/organism/SearchResult/SubHeader/SubHeader.module.scss';
 
 const SubHeader = () => {
+  const { selectedFilters } = useFilter();
   return (
     <div className={style.SubHeaderContainer}>
       <div className={style.SubHeader}>
-        <span className={style.PageTitle}>iPhone iOS cep telefonu</span>
+        <span className={style.PageTitle}>Cep Telefonu Fiyatları ve Modelleri</span>
         <span className={style.SearchTextContainer}>
-          <span>{'Aranan Kelime: '}</span>
-          <span className={style.SearchText}>iphone 11</span>
+          {selectedFilters?.search && (
+            <>
+              <span>{'Aranan Kelime: '}</span>
+              <span className={style.SearchText}>{selectedFilters?.search}</span>
+            </>
+          )}
         </span>
       </div>
       <div>
