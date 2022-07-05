db20d17da8d72c2f12c938e15851af04211332dc
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-06 01:59:32 +0300

    Refactor search result and product list components

--- src/components/organism/SearchResult/ProductList/ProductList.js
+++ src/components/organism/SearchResult/ProductList/ProductList.js
@@ -5,52 +5,10 @@ import Pagination from 'components/organism/Pagination/Pagination';
 import { useFilter, useProducts } from 'hooks';
 
 import styles from 'components/organism/SearchResult/ProductList/ProductList.module.scss';
-import Search from 'assets/Search.svg';
-import Spinner from 'assets/Spinner.svg';
-import NoResult from 'assets/NoResult.svg';
 
-const ProductList = memo(() => {
-  const { products, isLoading, isError } = useProducts();
+const ProductList = memo(({ results }) => {
+  const { products } = useProducts();
   const { selectedFilters, setSelectedFilters } = useFilter();
-
-  const lastIndex = selectedFilters.page * 12;
-  const firstIndex = lastIndex - 12;
-  const results = products?.length < 12 ? products : products?.slice(firstIndex, lastIndex);
-
-  if (isLoading) {
-    return (
-      <div className={styles.NoResultWrapper}>
-        <div className={styles.NoResultContainer}>
-          <img src={Spinner} alt="Spinner" width={72} height={72} />
-          <span className={styles.NoResultText}>Yükleniyor</span>
-        </div>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className={styles.NoResultWrapper}>
-        <div className={styles.NoResultContainer}>
-          <img src={NoResult} alt="No Product" width={72} height={72} />
-          <span className={styles.NoResultText}>Hata! Üzgünüz bir sorunla karşılaştık.</span>
-        </div>
-      </div>
-    );
-  }
-
-  if (results.length === 0) {
-    return (
-      <div className={styles.NoResultWrapper}>
-        <div className={styles.NoResultContainer}>
-          <img src={Search} alt="No Product" width={72} height={72} />
-          <span className={styles.NoResultText}>
-            {selectedFilters?.search} ile ilgili sonuç bulunamamıştır.
-          </span>
-        </div>
-      </div>
-    );
-  }
 
   const paginate = pageNumber => {
     setSelectedFilters({ ...selectedFilters, page: pageNumber });
--- src/components/organism/SearchResult/SearchResult.js
+++ src/components/organism/SearchResult/SearchResult.js
@@ -2,16 +2,66 @@ import { memo } from 'react';
 import SideBar from 'components/organism/SearchResult/SideBar/SideBar';
 import ProductList from 'components/organism/SearchResult/ProductList/ProductList';
 import SubHeader from 'components/organism/SearchResult/SubHeader/SubHeader';
+import { useProducts, useFilter } from 'hooks';
 
 import style from 'components/organism/SearchResult/SearchResult.module.scss';
+import Spinner from 'assets/Spinner.svg';
+import NoResult from 'assets/NoResult.svg';
+import Search from 'assets/Search.svg';
 
 const SearchResult = memo(() => {
+  const { products, isLoading, isError } = useProducts();
+  const { selectedFilters } = useFilter();
+
+  const lastIndex = selectedFilters.page * 12;
+  const firstIndex = lastIndex - 12;
+  const results = products?.length < 12 ? products : products?.slice(firstIndex, lastIndex);
+
+  if (results.length === 0) {
+    return (
+      <div className={style.NoResultWrapper}>
+        <div className={style.NoResultContainer}>
+          <img src={Search} alt="No Product" width={72} height={72} />
+          <span className={style.NoResultText}>
+            {selectedFilters?.search ? (
+              <p>{selectedFilters?.search} ile ilgili sonuç bulunamamıştır.</p>
+            ) : (
+              <p>Sonuç bulunamadı.</p>
+            )}
+          </span>
+        </div>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className={style.NoResultWrapper}>
+        <div className={style.NoResultContainer}>
+          <img src={Spinner} alt="Spinner" width={72} height={72} />
+          <span className={style.NoResultText}>Yükleniyor</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className={style.NoResultWrapper}>
+        <div className={style.NoResultContainer}>
+          <img src={NoResult} alt="No Product" width={72} height={72} />
+          <span className={style.NoResultText}>Hata! Üzgünüz bir sorunla karşılaştık.</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={style.searchResultWrapper}>
       <SubHeader />
       <div className={style.filterAndProducts}>
         <SideBar />
-        <ProductList />
+        <ProductList results={results} />
       </div>
     </div>
   );
