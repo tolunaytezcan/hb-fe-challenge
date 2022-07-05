a60909afbb4f6ff79ad38aa3e06710e0e1f9dcd6
Tolunay Tezcan <ttezcan@ebay.com>
2022-07-06 01:34:11 +0300

    Add uselocation to dynamic link

--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -1,9 +1,14 @@
-import { createContext, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { createContext, useState, useEffect, useMemo } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const FilterContext = createContext();
 
 export default FilterContext;
+
+function useQuery() {
+  const { search } = useLocation();
+  return useMemo(() => new URLSearchParams(search), [search]);
+}
 
 export const FilterProvider = ({ children }) => {
   const history = useHistory();
@@ -18,16 +23,32 @@ export const FilterProvider = ({ children }) => {
   const clearFilters = () => {
     setSelectedFilters({
       color: '',
-      brand: '',
       sort: '',
       search: '',
+      brand: '',
       page: 1
     });
   };
 
+  let query = useQuery();
+
+  const colorQuery = query.get('color');
+  const brandQuery = query.get('brand');
+  const sortQuery = query.get('sort');
+  const searchQuery = query.get('search');
+  const pageQuery = query.get('page');
+
+  useEffect(() => {
+    setSelectedFilters(prev => ({ ...prev, brand: brandQuery }));
+    setSelectedFilters(prev => ({ ...prev, color: colorQuery }));
+    setSelectedFilters(prev => ({ ...prev, sort: sortQuery }));
+    setSelectedFilters(prev => ({ ...prev, search: searchQuery }));
+    setSelectedFilters(prev => ({ ...prev, page: +pageQuery }));
+  }, []);
+
   useEffect(() => {
     history.push(
-      `/?brand=${selectedFilters.brand}&color=${selectedFilters.color}&sort=${selectedFilters.sort}&search=${selectedFilters.search}&_limit=12&page=${selectedFilters.page}`
+      `/?brand=${selectedFilters.brand}&color=${selectedFilters.color}&sort=${selectedFilters.sort}&search=${selectedFilters.search}&page=${selectedFilters.page}`
     );
   }, [selectedFilters]);
 
